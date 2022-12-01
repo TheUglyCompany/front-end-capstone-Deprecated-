2a4d7dde2ee56120424d3d3322e739107a3459d0
sindwarf <sindorfsolutions@gmail.com>
2022-11-30 17:41:35 -0800

    setup eslint

---
+++ client/src/components/App.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+// https://github.com/airbnb/javascript/tree/master/react
+
+function App() {
+  return <div>Hello World</div>;
+}
+
+export default App;
--- client/src/index.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-
-let App = () => {
-  return(<div>Hello World</div>);
-};
-
-ReactDOM.render(<App/>, document.getElementById('root'));
